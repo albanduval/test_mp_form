9ba14c5568939b31d4c49c3489db92b0108b5a42
Alban Duval <alban@chamo.fr>
2014-11-18 00:47:30 +0100

    add basic model classes & basic form integration

--- src/Chm/MPFormBundle/Controller/DefaultController.php
+++ src/Chm/MPFormBundle/Controller/DefaultController.php
@@ -3,11 +3,36 @@
 namespace Chm\MPFormBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Chm\MPFormBundle\Entity\Quotation;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('ChmMPFormBundle:Default:index.html.twig', array('name' => $name));
+    	$quotation = new Quotation();
+
+        $form = $this->createFormBuilder($quotation)
+            ->add('size', 'entity', array(
+				    'class'     => 'ChmMPFormBundle:Size',
+				    'property'  => 'name',
+				    'expanded'  => true,
+				    'multiple'  => false,
+				))
+            ->add('color', 'entity', array(
+				    'class'     => 'ChmMPFormBundle:Color',
+				    'property'  => 'name',
+				    'expanded'  => true,
+				    'multiple'  => false,
+				))
+            ->add('quantity', 'integer')
+            ->add('front_color_count', 'integer')
+            ->add('back_color_count', 'integer')
+            ->add('email', 'email')
+            ->add('save', 'submit', array('label' => 'Request a quotation'))
+            ->getForm();
+
+        return $this->render('ChmMPFormBundle:Default:index.html.twig', array(
+        	'form' => $form->createView(),
+        	));
     }
 }
---
+++ src/Chm/MPFormBundle/Entity/User.php
@@ -0,0 +1,274 @@
+<?php
+
+namespace Chm\MPFormBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address1", type="text")
+     */
+    private $address1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address2", type="text")
+     */
+    private $address2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zip_code", type="string", length=50)
+     */
+    private $zip_code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set address1
+     *
+     * @param string $address1
+     * @return User
+     */
+    public function setAddress1($address1)
+    {
+        $this->address1 = $address1;
+
+        return $this;
+    }
+
+    /**
+     * Get address1
+     *
+     * @return string 
+     */
+    public function getAddress1()
+    {
+        return $this->address1;
+    }
+
+    /**
+     * Set address2
+     *
+     * @param string $address2
+     * @return User
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    /**
+     * Get address2
+     *
+     * @return string 
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * Set zip_code
+     *
+     * @param string $zipCode
+     * @return User
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zip_code = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * Get zip_code
+     *
+     * @return string 
+     */
+    public function getZipCode()
+    {
+        return $this->zip_code;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return User
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
